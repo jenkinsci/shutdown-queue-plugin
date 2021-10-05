fdf2bf697b1decdd417727bc8d1bc461061922e1
Dominik Kozubik <kozubik.dominik@gmail.com>
2021-10-05 10:01:42 +0200

    Update EstimatedDurationComparator.java

--- src/main/java/cz/muni/fi/xkozubi1/EstimatedDurationComparator.java
+++ src/main/java/cz/muni/fi/xkozubi1/EstimatedDurationComparator.java
@@ -1,15 +1,30 @@
 package cz.muni.fi.xkozubi1;
 
 import hudson.model.Queue;
-import hudson.model.queue.AbstractQueueSorterImpl;
 
-import java.io.Serializable;
-import java.util.List;
+import java.util.Comparator;
 
-public class DefaultSorter extends AbstractQueueSorterImpl implements Serializable {
+public class EstimatedDurationComparator implements Comparator<Queue.BuildableItem> {
+
+    private static class EstimatedDurationComparatorHolder {
+        static final EstimatedDurationComparator INSTANCE = new EstimatedDurationComparator();
+    }
+
+    public static EstimatedDurationComparator getInstance() {
+        return EstimatedDurationComparatorHolder.INSTANCE;
+    }
 
     @Override
-    public void sortBuildableItems(List<Queue.BuildableItem> list) {
-        list.sort(this);
+    public int compare(Queue.BuildableItem buildableItem0, Queue.BuildableItem buildableItem1) {
+
+        if (buildableItem0.task.getEstimatedDuration() < buildableItem1.task.getEstimatedDuration()) {
+            return -1;
+        }
+
+        if (buildableItem0.task.getEstimatedDuration() > buildableItem1.task.getEstimatedDuration()) {
+            return 1;
+        }
+
+        return 0;
     }
 }
