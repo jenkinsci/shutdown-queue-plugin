c91816101e54fc340006f1ebc51634ceee7b77f5
dkozubik <73256062+dkozubik@users.noreply.github.com>
2021-10-05 09:56:39 +0200

    Create ShutdownQueueComputerListener.java

---
+++ src/main/java/cz/muni/fi/xkozubi1/ShutdownQueueComputerListener.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.plugin;
+
+import hudson.Extension;
+import hudson.model.Computer;
+import hudson.model.TaskListener;
+import hudson.slaves.ComputerListener;
+import jenkins.model.Jenkins;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Extension
+public class ShutdownQueueComputerListener extends ComputerListener {
+
+    @Override
+    public void onOnline(Computer c, TaskListener listener) {
+        onTemporarilyOnline(c);
+    }
+
+    @Override
+    public void onTemporarilyOnline(Computer computer) {
+        HandleQuietingDown logicClass = new HandleQuietingDown(computer);
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        executorService.scheduleAtFixedRate(() -> {
+            System.out.println("Run");
+            if (ShutdownQueueConfiguration.getInstance().getPluginOn() && Jenkins.getInstanceOrNull().isQuietingDown()
+                    && !Jenkins.getInstanceOrNull().getQueue().isEmpty()) {
+                logicClass.handleLogic();
+            }
+        }, 1, 1, TimeUnit.SECONDS);
+
+        System.out.println("Thread started");
+    }
+}
