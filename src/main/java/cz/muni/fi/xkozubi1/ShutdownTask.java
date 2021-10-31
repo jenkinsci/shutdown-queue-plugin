7d5a5996eb418529e04a2e725cbad627f6aff626
Dominik Kozubik <kozubik.dominik@gmail.com>
2021-10-31 14:10:30 +0100

    added logger

--- src/main/java/cz/muni/fi/xkozubi1/ShutdownTask.java
+++ src/main/java/cz/muni/fi/xkozubi1/ShutdownTask.java
@@ -3,9 +3,11 @@ package cz.muni.fi.xkozubi1;
 import hudson.model.Computer;
 import jenkins.model.Jenkins;
 
+import java.util.logging.Logger;
+
 
 public class ShutdownTask implements Runnable {
-
+    private static Logger logger = Logger.getLogger(ShutdownTask.class.getName());
     private HandleQuietingDown handleQuietingDown;
 
     public ShutdownTask(Computer computer) {
@@ -14,9 +16,10 @@ public class ShutdownTask implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Run");
-        if (ShutdownQueueConfiguration.getInstance().getCheckboxPlugin() && Jenkins.get().isQuietingDown()
-                && Jenkins.get().getQueue().getBuildableItems().size() > 0) {
+        logger.info("Run");
+        if (ShutdownQueueConfiguration.getInstance().getCheckboxPlugin() &&
+                Jenkins.get().isQuietingDown() &&
+                Jenkins.get().getQueue().getBuildableItems().size() > 0) {
             try {
                 handleQuietingDown.handleLogic();
             } catch (InterruptedException e) {
